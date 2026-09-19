f908c091f5c6fd299f16fe2bb1277a6633a4d393
zzahrina455-design <zzahrina455@gmail.com>
2026-09-20 02:40:08 +0700

    up

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  test: {
+  test : {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
